8655010cd807c4f8e34cd852f81d35b220034f0f
conor <conor.m@me.com>
2021-11-09 17:25:59 +0000

    getAll for classes done and working

--- src/resources/classes/router.js
+++ src/resources/classes/router.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
-const { } = require("./controller")
+const { getAll } = require("./controller")
+
+const router = express.Router();
+
+router.get("/", getAll)
 
 module.exports = router;
