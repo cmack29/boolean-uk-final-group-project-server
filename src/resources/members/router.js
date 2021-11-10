6258b5d175529640017180b7f880fce46604dfc5
ValentinNM <valentinm.web@gmail.com>
2021-11-10 14:05:52 +0200

    UPDATE: Full-CRUD-Completed-For-Memebers_model

--- src/resources/members/router.js
+++ src/resources/members/router.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getAllMembers } = require("./controller")
+const {
+    getAllMembers,
+    addNewMember,
+    removeOneMemeber,
+    updateOneMember,
+    getOneMmber
+} = require("./controller")
+
+router.get("/:id", getOneMmber);
 
 router.get("/", getAllMembers);
+
+router.post("/", addNewMember);
+
+router.delete("/:id", removeOneMemeber)
+
+router.patch("/:id", updateOneMember)
 
 module.exports = router;
